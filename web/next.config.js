e4013b3c19c181fc1198460892f41e49944ae935
Mike Holloway <me@mikeholloway.co.uk>
2022-09-17 16:10:25 +0100

    bumped deps

--- web/next.config.js
+++ web/next.config.js
@@ -1,19 +1,10 @@
 const { withExpo } = require('@expo/next-adapter');
 const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')([
-  'react-native-web',
-  'react-native-svg',
-  'native-base',
-  'react-native-svg',
-]);
+const withTM = require('next-transpile-modules')(['react-native-web', 'react-native-svg', 'native-base']);
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {};
 
-module.exports = withPlugins(
-  [
-    withTM,
-    [withExpo, { projectRoot: __dirname }],
-    // your plugins go here.
-  ],
-  nextConfig,
-);
+module.exports = withPlugins([withTM, [withExpo, { projectRoot: __dirname }]], nextConfig);
